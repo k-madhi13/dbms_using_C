51a484b7e0cc349833c0541210e0fa75d4b7cff5
ishabudhathoki9-beep <ishabudhathoki9@gmail.com>
2026-04-27 19:50:14 +0545

    Update delete.h

--- delete.h
+++ delete.h
@@ -3,5 +3,7 @@
 
 void delete_table(char *db_name, char *table_name);
 void delete_database(char *db_name);
+void delete_column(char *db_name, char *table_name, char *column);
+void delete_row(char *db_name, char *table_name, int row_number);
 
 #endif
