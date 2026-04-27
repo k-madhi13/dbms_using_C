f0d2b1b6036560016337b0465e1468667afad168
ishabudhathoki9-beep <ishabudhathoki9@gmail.com>
2026-04-27 19:48:34 +0545

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "database.h"
-
 int main() {
     int choice;
     char db_name[100];
     char table_name[100];
+    char column[100];
+    char value[100];
+    int row_number;
 
-    printf("1. Delete Table\n");
+    printf("1. Delete Row (by condition)\n");
     printf("2. Delete Database\n");
+    printf("3. Delete Column\n");
+    printf("4. Delete Row (by row number)\n");
+    printf("Enter choice: ");
     scanf("%d", &choice);
 
     if (choice == 1) {
@@ -15,16 +20,39 @@ int main() {
         scanf("%s", db_name);
         printf("Enter table name: ");
         scanf("%s", table_name);
-        delete_table(db_name, table_name);
+        printf("Enter column: ");
+        scanf("%s", column);
+        printf("Enter value: ");
+        scanf("%s", value);
+        delete_table(db_name, table_name, column, value);
 
     } else if (choice == 2) {
         printf("Enter database name: ");
         scanf("%s", db_name);
         delete_database(db_name);
 
+    } else if (choice == 3) {
+        printf("Enter database name: ");
+        scanf("%s", db_name);
+        printf("Enter table name: ");
+        scanf("%s", table_name);
+        printf("Enter column name to delete: ");
+        scanf("%s", column);
+        delete_column(db_name, table_name, column);
+
+    } else if (choice == 4) {
+        printf("Enter database name: ");
+        scanf("%s", db_name);
+        printf("Enter table name: ");
+        scanf("%s", table_name);
+        printf("Enter row number to delete (1 = first data row): ");
+        scanf("%d", &row_number);
+        delete_row(db_name, table_name, row_number);
+
     } else {
         printf("Invalid choice.\n");
     }
 
     return 0;
+}
 }
